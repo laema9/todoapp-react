313fad86df2dad86c169c20dd67bbc43bbcc73e0
laema9 <laema9@gmail.com>
2025-06-17 18:50:54 +0200

    feat(pwa): pwa integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,44 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true, 
+      },
+      manifest: {
+        name: "Todo Dashboard",
+        short_name: "Todo",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "#000000",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "public/icons/favicon-32x32",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "public/icons/favicon-32x32",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
